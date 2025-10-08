344e1199d9247b50455f927cbe05cdce1d92539a
John DeConti <captainlinky@gmail.com>
2025-10-08 12:52:33 -0400

    fixing app name in batch script.

--- build_call_button.bat
+++ build_call_button.bat
@@ -26,7 +26,7 @@ IF ERRORLEVEL 1 (
 
 :: Build the EXE
 echo 🚧 Building Call_Button_App executable...
-pyinstaller --onefile --windowed Call_Button_App.py ^
+pyinstaller --onefile --windowed app.py ^
   --add-data "templates;templates" ^
   --add-data "static;static"
 
